bc4520d142ae05312387b2792734001b0114618a
julia smith <julia.smith@us.sogeti.com>
2019-06-14 23:47:11 -0500

    Assignment 1

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,42 +1,58 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu Jun 13 13:19:37 2019
+Created on Fri Jun 14 21:34:00 2019
 
 @author: julsmith
 """
-
 import os
 import csv
 ## find csv path 
-pybank_csv = os.path.join("python-challenge","PyBank","Pybank_budget_data.csv")
-## read the file
-with open(pybank_csv, newline = "") as csvfile:
-    csvreader = csv.reader(csvfile, delimiter=",")
-    print(csvreader)
-    
-    ## print file 
-with open(pybank_csv, 'r') as text:
-    print(text)
-    lines = text.read()
-    print(lines)    
+pybank_csv = os.path.join("Pybank_budget_data.csv")
 
-def print_data(PyBank_data):
-    date = int(PyBank_data[0])
-    profit_losses = int(PyBank_dat[1])
-    
-    ## get number of rows = months
-    
+
+# read file 
 with open(pybank_csv, "r") as f:
     reader = csv.reader(f,delimiter = ",")
     data = list(reader)
+    ## calculate total months
     row_count = len(data)-1
-    print(row_count)
+    #print(data)
     
-total_months = (row_count)
-print("Financial Analysis")
-print("-----------------------------")
-print("Total Months: " + str(total_months))
+    total = len(data)-1
+    #print(total)
+    
+index = 0
+sum = 0
+sum_of_average = 0
+i = 1
+difference = 0
+difference_array = []
+while i<len(data):
+    ## print(i)
+    ## print(data[i][1])
+    sum += int(data[i][1])
+    if i+1 <= total:
+        difference = int(data[i+1][1]) - int(data[i][1])      
+        print(difference)
+        difference_array.append(difference)
+        sum_of_average += difference        
+    i += 1
+    
+#print(sum)
 
-with open(pybank_csv) as fin:
-    total = sum(int(r[1] for r in csv.reader(fin))
+#print(sum_of_average/(total-1)) 
+#print(difference_array)
+max_val = max(difference_array)
+max_index = difference_array.index(max_val)
+min_val = min(difference_array)
+min_index = difference_array.index(min_val)
+
+print("Financial Analysis")
+print("-------------------")
+print("Total Months: " + str(total))
+print("Total: " + str(sum))
+print("Average Change: " + str(sum_of_average))
+print("Greatest Increase in Profits: " +  str(data[max_index+2][0]) + " ($" +(str(max_val)) + ")" )
+print("Greatest Decrease in Profits: " +  str(data[min_index+2][0]) + " ($" +(str(min_val)) + ")" )
+    
     
